4232fd50f5ebb7513d91923a30e0a607c32d1fb1
codingcele <marcocelegato9@gmail.com>
2023-05-10 20:15:43 +0200

    validazioni (messaggio The value '' is invalid nel price sostituito dal messaggio impostato da me mettendo price nullable nel model)

--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -10,10 +10,22 @@ namespace la_mia_pizzeria_static
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
         public string Image { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(50, ErrorMessage = "Il nome non può avere più di 50 caratteri")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(200, ErrorMessage = "Gli ingredienti non possono avere più di 200 caratteri")]
+        [IngredientsMin3Words(ErrorMessage = "Gli ingredienti devono contenere almeno 3 parole.")]
         public string Ingredients { get; set; }
-        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Range(0.01, 25.00, ErrorMessage = "Il prezzo deve essere positivo e minore di 25.")]
+        public decimal? Price { get; set; }
 
         public Pizza()
         {
---
+++ la-mia-pizzeria-static/Validations/IngredientsMin3Words.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static
+{
+    public class IngredientsMin3Words : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+            {
+                var ingredients = (string)value;
+                var words = ingredients.Split(' ');
+                return words.Length >= 3;
+            }
+            return false;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return $"{name} deve contenere almeno 3 parole.";
+        }
+    }
+}
